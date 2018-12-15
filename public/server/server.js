cac04a59ea0a0e4dac63bfec3f39c1e77d5242d3
KryptoCrash <kryptocrashmh@gmail.com>
2018-12-15 18:08:04 -0500

    test

--- public/server/server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io').listen(server);
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
