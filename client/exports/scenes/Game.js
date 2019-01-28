cbfc421950661fe3af2da9868960af53f999e119
PatheticMustan <patheticmustan@gmail.com>
2019-01-27 21:09:02 -0500

    fuck, backgrounds suck.

--- client/exports/scenes/Game.js
+++ client/exports/scenes/Game.js
@@ -2,20 +2,20 @@ import Player from "../models/player.js";
 
 export default class Game extends Phaser.Scene {
     constructor() {
-        super('Game')
+        super('Game');
     }
     preload() {
-        this.load.image('background', './client/assets/backgroundtest.png');
+        this.load.image('background', './client/assets/Test_Map.svg');
         this.load.svg('player', './client/assets/Character.svg');
     }
     create() {
-        var background = this.physics.add.sprite(0,0,'background');
+        var background = this.physics.add.sprite(0, 0,'background');
         var cammy = this.cameras.main;
-        var player = new Player(this,cammy);
+        var player = new Player(this, cammy);
         player.create();
         this.input.on('pointermove', (pointer) => {
-            player.movePlayer(pointer.x,pointer.y);
-        }, this)
+            player.movePlayer(pointer.x, pointer.y);
+        }, this);
 
     }
     update() {
