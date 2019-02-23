42410575c60a880177c782ab2df25e7a7b3f08b8
Daniel Nakushnov <daniel.nakushnov@gmail.com>
2019-02-23 02:47:31 +0200

    Made daggers spawn in the right hand

--- server/models/weapons/bullet.js
+++ server/models/weapons/bullet.js
@@ -1,11 +1,14 @@
 var calcMoveWithAngle = require("../../events/calcMoveWithAngle");
+let rotatePoint = require("../../events/rotatePoint");
+
 module.exports = class Bullet {
     constructor(player, id) {
         this.player_id = player.id;
         this.id = id;
+        let pos = rotatePoint(player.x, player.y, player.angle, player.x + player.props.hand1.x, player.y + player.props.hand1.y)
         this.angle = player.angle;
-        this.x = player.x;
-        this.y = player.y;
+        this.x = pos.x;
+        this.y = pos.y;
         this.vx = 0;
         this.vy = 0;
         this.type = player.hero.bullet;
